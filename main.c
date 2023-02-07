1f92f9f7b00d9f2afffeef3cec28abf121d138cb
srikavyachallapilla <39005729+srikavyachallapilla@users.noreply.github.com>
2023-02-06 23:36:46 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,17 +8,32 @@
 
 //Declaring function collatzConjucture
 void collatzConjucture(int n, int child){
+    //declare char type variable to store a string
+    char buffer[50];
+    //using sprintf to store the string into buffer instead of printing on stdout.
+    sprintf(buffer, "From Child %d init n = %d, ", child, n);
+    //displaying the buffer using printf
+    printf("%s", buffer);
     //iterating loop while the condition is true for collatz conjucture
     while(n > 1){
         if(n % 2 == 0){ //collatz conjucture logic for even numbers 
             n = n / 2;
+            //using sprintf to store the string into buffer instead of printing on stdout.
+            sprintf(buffer, "From child %d n = %d, ", child, n);
+            //displaying the buffer using printf
+            printf("%s", buffer);
         }
         else{
             //collatz conjucture logic for odd numbers 
             n = (n * 3) + 1;
+            //using sprintf to store the string into buffer instead of printing on stdout.
+            sprintf(buffer, "From child %d n = %d, ", child, n);
+            //displaying the buffer using printf
+            printf("%s", buffer);
         }
-        printf("From Child %d n=%d, ",child, n);
     }
+    //displaying in new line
+    printf("\n");
 }
 
 //declaring main function with argc and argv as command line arguments for command line input
@@ -28,8 +43,6 @@ int main(int argc, char* argv[])
     int pid, pid1;
     //converting char type variable to int type
     int a = atoi(argv[1]);
-    //declare char type variable to store a string
-    char buffer[50];
     //fork() is called to create new child process
     pid = fork();
     //checking if the input is in given limit
@@ -37,23 +50,15 @@ int main(int argc, char* argv[])
         //if fork() returns 0, it means child process is created.
         if(pid == 0){
             //child 1 calling the collatzConjucture function
-            //using sprintf to store the string into buffer instead of printing on stdout.
-            sprintf(buffer, "From Child 1 init n = %d, ", a);
-            //displaying the buffer using printf
-            printf("%s", buffer);
             collatzConjucture(a, 1);
-            printf("\n");
             exit(0);
         }
+        else{
             //calling fork() to create another child process
             pid1 = fork();
             //if fork() returns 0, it means another child process is created
             if(pid1 == 0){
                 //child 2 calling the collatzConjucture function
-                //using sprintf to store the string into buffer instead of printing on stdout.
-                sprintf(buffer, "From Child 2 init n = %d, ", a+3);
-                //displaying the buffer using printf
-                printf("%s", buffer);
                 collatzConjucture(a+3, 2);
                 exit(0);
             }
@@ -64,5 +69,8 @@ int main(int argc, char* argv[])
                 printf("children complete\n");
             }
         }
+    }
+    
+
     return 0;
 }
